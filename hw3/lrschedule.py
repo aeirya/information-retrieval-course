121f659578a15674f4da56c1a2bf431451c8d694
aeirya <aeirya@gmail.com>
2024-02-07 14:01:45 +0330

    update lr scheduler

--- hw3/lrschedule.py
+++ hw3/lrschedule.py
@@ -1,6 +1,9 @@
 import numpy as np
 
 def acc_scheduler(n_epochs, init, end):
+    if init == end:
+        return []
+
     X = np.linspace(-2, -1, n_epochs, True)
     B = (2*init - end) / (end - init)
     A = end * (B + 1)
@@ -10,8 +13,9 @@ def learning_rate_scheduler(
     n_epochs,
     init_lr = 0.05,
     end_lr = 0.0001,
-    warmup = 0.2,
-    x = 0.5
+    warmup = 0.01,
+    x = 0.1,
+    reach_endpoint = False
 ):
     w_init = init_lr + (1 - x) * (end_lr - init_lr)
     n_warmup = int(n_epochs * warmup)
@@ -23,8 +27,12 @@ def learning_rate_scheduler(
     n_epochs -= n_warmup
 
     # exponential decrease 
-    n_exp = n_epochs*3//4
-    x = np.exp(np.linspace(0.0, -2.0, n_exp, True))
+    if not reach_endpoint:
+        n_exp = n_epochs*3//4
+    else:
+        n_exp = n_epochs
+
+    x = np.exp(np.linspace(0.0, -3.0, n_exp, True))
     x = init_lr + (1.0 - x) * (end_lr-init_lr)
 
     for lr in x:
@@ -32,9 +40,10 @@ def learning_rate_scheduler(
 
     n_epochs -= n_exp
     
-    # linear tail
-    for lr in np.linspace(x[-1], end_lr, n_epochs, True):
-        yield lr
+    if reach_endpoint:
+        # linear tail
+        for lr in np.linspace(x[-1], end_lr, n_epochs, True):
+            yield lr
 
     return None
 
