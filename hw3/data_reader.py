55440c89cfc1cd40085c8031944fd1ff21b23ac2
aeirya <aeirya@gmail.com>
2024-02-15 00:50:21 +0330

    extract top k from matrix

--- hw3/data_reader.py
+++ hw3/data_reader.py
@@ -2,19 +2,19 @@ import pandas as pd
 import numpy as np
 
 def read_df(ds_type):
+
     if ds_type not in ['train', 'test']:
+        print("invalid dataset")
         return
     
     ds_type = f'dataset/{ds_type}_dataset.txt'
-
     df = pd.read_csv(ds_type, delimiter='\t')
     
     return df
 
 
-def read_interaction_matrix(dataset = 'train'):
-    df = read_df(dataset)
-    
+def interaction_matrix(df, return_id_maps=True):
+
     users = sorted(set(df['user_id']))
     items = sorted(set(df['item_id']))
 
@@ -25,8 +25,19 @@ def read_interaction_matrix(dataset = 'train'):
     R = np.zeros((len(users), len(items)))
 
     for user, item in grouping.groups.keys():
+        print(user, item)
         user_id = user_to_id[user]
         item_id = item_to_id[item]
         R[user_id, item_id] = 1
 
+    if return_id_maps:
+        return user_to_id, item_to_id, R
+
     return R
+
+
+def read_interaction_matrix(dataset = 'train', return_id_maps=False):
+    
+    df = read_df(dataset)
+    
+    return interaction_matrix(df, return_id_maps)
