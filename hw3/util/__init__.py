8efb14b52f2a3f54839e5d52bf7b6623c1e3f208
aeirya <aeirya@gmail.com>
2024-02-19 21:00:44 +0330

    final release, add visualisation of hits

--- hw3/util/__init__.py
+++ hw3/util/__init__.py
@@ -2,9 +2,45 @@ from .data_reader import train_test_df, interaction_matrix, train_test_mtx, read
 from .evaluation import evaluate, topk
 from .plot import heatmaps, heatmap
 
+import numpy as np
+
 
 # df_test = eval_init()
 evaluation.init()
 
 # def easy_eval(M):
 #     return evaluate(M, df_test)
+
+
+def repeat_like(a, M):
+    '''
+    a(n,) and M(n,m)
+    '''
+
+    a = np.repeat(a, M.shape[1]).reshape((a.shape[0], -1))
+    return a
+
+
+def normal(M):
+    a = ((M.min(axis=1) == 0) & (M.max(axis=1) == 0))
+    
+    M = M.copy()
+
+    M -= repeat_like(M.min(axis=1), M)
+    M[~a, :] /= repeat_like(M.max(axis=1), M)[~a, :]
+    
+    M = np.where(repeat_like(a, M), 0, M)
+
+    return M
+
+def normal_exp(M):
+    '''
+    map values to the [0,1] range
+    '''
+
+    M = M.copy()
+    M = normal(M)
+    M = np.exp(M)
+
+    return normal(M)
+
--- hw3/util/plot.py
+++ hw3/util/plot.py
@@ -1,15 +1,15 @@
 def heatmap(a):
     import matplotlib.pyplot as plt
     plt.imshow(a, cmap='hot', interpolation='nearest')
-    plt.show()
+    # plt.show()
 
 
-def heatmaps(iterable):
+def heatmaps(iterable, devide_n = 0.5):
     import numpy as np
     from functools import reduce
 
     n = iterable[0].shape[0]
-    D = np.ones((n, 1)) * 0.5
+    D = np.ones((n, 1)) * devide_n
     
     t = reduce(lambda a,b:a+b, [[M, D] for M in iterable], [])
     T = np.concatenate(t, axis=1)
